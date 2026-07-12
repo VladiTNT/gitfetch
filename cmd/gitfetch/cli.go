e2075bb9bb62323caa94e4950ed47584013a9b8f
olvyyd <olvyyd.orca@gmail.com>
2026-07-12 12:07:29 +0300

    made langague makeup

--- cmd/gitfetch/cli.go
+++ cmd/gitfetch/cli.go
@@ -10,6 +10,7 @@ type Cli struct {
 	AvgChars *bool
 	NoSize   *bool
 	NoLines  *bool
+	Nolang   *bool
 }
 
 func NewCli() *Cli {
@@ -18,6 +19,7 @@ func NewCli() *Cli {
 		AvgChars: flag.Bool("avgchars", false, "average amount of chars per line"),
 		NoSize:   flag.Bool("nosize", false, "disable total project size"),
 		NoLines:  flag.Bool("nolines", false, "disable total project lines"),
+		Nolang:   flag.Bool("noalng", false, "disable language makeup"),
 	}
 
 	flag.Parse()
@@ -41,6 +43,14 @@ func (c *Cli) Exec() error {
 
 	if !*c.NoLines {
 		fmt.Printf("Total lines: %d.\n", GetTotalLines(files))
+	}
+
+	if !*c.Nolang {
+		langs, size := GetLanguageMakeup(files)
+
+		for k, v := range langs {
+			fmt.Printf("'%s' files are %f%%.\n", k, float64(v)/float64(size)*100)
+		}
 	}
 
 	if *c.AvgChars {
--- cmd/gitfetch/file_info.go
+++ cmd/gitfetch/file_info.go
@@ -82,3 +82,19 @@ func GetTotalAvgChars(files []*FileInfo) float64 {
 
 	return totalAvgChars / float64(len(files))
 }
+
+func GetLanguageMakeup(files []*FileInfo) (map[string]int64, int64) {
+	langs := make(map[string]int64)
+
+	for _, file := range files {
+		langs[file.Extension] += file.Size
+	}
+
+	var size int64
+
+	for _, v := range langs {
+		size += v
+	}
+
+	return langs, size
+}
